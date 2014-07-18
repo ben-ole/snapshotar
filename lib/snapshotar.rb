c98d903f6848a2079494a0284cc24f97e67b8dd8
Benjamin Müller <benjamin@urbn.de>
2014-07-18 18:26:12 +0200

    started with s3 integration

--- lib/snapshotar.rb
+++ lib/snapshotar.rb
@@ -1,19 +1,29 @@
 require "snapshotar/version"
+require "aws-sdk"
 
 module Snapshotar
 
   class S3Storage
 
     def initialize
-      @s3 = AWS::S3.new(
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
 
-      @bucket = s3.buckets[ENV['AWS_SNAPSHOTAR_BUCKET']]
+      raise ArgumentError, "You should set ENV['AWS_ACCESS_KEY_ID'] to a valid value" unless ENV['AWS_ACCESS_KEY_ID']
+      raise ArgumentError, "You should set ENV['AWS_SECRET_ACCESS_KEY'] to a valid value" unless ENV['AWS_SECRET_ACCESS_KEY']
+      raise ArgumentError, "You should set ENV['AWS_SNAPSHOTAR_BUCKET'] to a aws bucket name used only for snapshotting" unless ENV['AWS_SNAPSHOTAR_BUCKET']
+
+      p "Running S3 with key: #{ENV['AWS_ACCESS_KEY_ID']}, secret: #{ENV['AWS_SECRET_ACCESS_KEY']}, bucket: #{ENV['AWS_SNAPSHOTAR_BUCKET']}"
+
+      @s3 = AWS::S3.new(
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'])
+
+      @bucket = @s3.buckets[ENV['AWS_SNAPSHOTAR_BUCKET']]
     end
 
     def list
-
+      @bucket.objects.with_prefix('subdomains_dump').each do |obj|
+        puts obj.key
+      end
     end
 
   end
--- spec/s3storage_spec.rb
+++ spec/s3storage_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
+require 'dotenv'
+Dotenv.load
 
 describe Snapshotar::S3Storage do
-  pending "write it"
+  context "setup s3 connection" do
+
+    before do
+      @s3Storage = described_class.new
+    end
+
+    it "should initialize correctly" do
+    end
+
+    it "should list objects" do
+      @s3Storage.list
+    end
+
+  end
 end
