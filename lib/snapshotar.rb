075d80deefadea8d47d75debd44cdc771c6d365f
Benjamin Müller <elchbenny@googlemail.com>
2014-07-24 20:15:47 +0200

    added some documentation

--- lib/snapshotar.rb
+++ lib/snapshotar.rb
@@ -1,10 +1,26 @@
 require "snapshotar/version"
 require "snapshotar/core"
 
+##
+# Make a snapshot of your staging environment and pull back on your dev machine.
+
 module Snapshotar
 
   # Configuration
   class << self
+
+    ##
+    # Configuration object. Create a *snapshotar.rb* initializer under
+    # config/initializers and add your configuation inside:
+    #
+    # => Snapshotar.configure do |config|
+    # =>    config.storage_type = :s3
+    #
+    #       config.models << [Event, :name, :date]
+    #       config.models << [Artist, :name]
+    # => end
+
+    end
     attr_accessor :configuration
   end
 
